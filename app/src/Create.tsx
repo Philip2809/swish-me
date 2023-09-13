ee5d31392181d45f8de598af96dcb1dfdca4f233
Philip Magyar <philipandremagyar@gmail.com>
2023-09-13 15:34:55 +0200

    title & qr code

--- app/src/Create.tsx
+++ app/src/Create.tsx
@@ -8,6 +8,7 @@ import QrCode2Icon from '@mui/icons-material/QrCode2';
 import ShareIcon from '@mui/icons-material/Share';
 import { validate } from './helper';
 import { useState } from 'react';
+import QRCode from 'qrcode'
 
 const SwishMeInput = styled(TextField)({
   '&': {
@@ -52,13 +53,15 @@ const SwishMeButton = styled(Button)({
   },
 })
 
+
+
 function Create() {
 
   const [randomEnabled, setRandomEnabled] = useState(false);
 
   return (
     <>
-    {randomEnabled}
+      {randomEnabled}
       <SwishMeInput id='number' label="Nummer" inputProps={{ inputMode: 'numeric', placeholder: '0760123456', maxLength: 10 }} defaultValue={localStorage.getItem('number') || ''} />
       <SwishMeInput id='amt' label="Belopp" inputProps={{ inputMode: 'numeric', placeholder: '69.420' }} />
       <FormControlLabel control={<SwishMeCheckbox id='editAmt' />} label="Tillåt redigering" />
@@ -66,14 +69,35 @@ function Create() {
       <FormControlLabel control={<SwishMeCheckbox id='randomMsg' onChange={(v) => setRandomEnabled(v.target.checked)} />} label="Random meddelande" />
       <FormControlLabel control={<SwishMeCheckbox id='editMsg' />} label="Tillåt redigering" />
 
+      <canvas id='test' height={200} width={200}></canvas>
+
       <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
         <SwishMeButton
           variant="contained"
           startIcon={<QrCode2Icon />}
+          onClick={() => {
+            const canvas = document.getElementById('test') as HTMLCanvasElement | undefined;
+            if (!canvas) return;
+            const number = (document.getElementById('number') as HTMLInputElement).value;
+            const amt = (document.getElementById('amt') as HTMLInputElement).value;
+            const msg = (document.getElementById('msg') as HTMLInputElement).value;
+
+            if (validate(number, amt, msg)) {
+              const editAmt = (document.getElementById('editAmt') as HTMLInputElement).checked;
+              const editMsg = (document.getElementById('editMsg') as HTMLInputElement).checked;
+              const edit = [];
+              if (editAmt) edit.push('a');
+              if (editMsg) edit.push('m');
+
+              localStorage.setItem('number', number);
+
+              QRCode.toCanvas(canvas, `${location.origin}/?n=${number}&a=${amt}${randomEnabled ? '&r=1' : msg ? `&m=${msg}` : ''}${edit.length > 0 ? `&e=${edit.join()}` : ''}`)
+            }
+          }}
         >
           Qr kod
         </SwishMeButton>
-        { /* *Can be undefined, eg firefox windows */ }
+        { /* *Can be undefined, eg firefox windows */}
         {(navigator?.share as (ShareData | undefined)) && <SwishMeButton
           variant="contained"
           startIcon={<ShareIcon />}
@@ -81,7 +105,7 @@ function Create() {
             const number = (document.getElementById('number') as HTMLInputElement).value;
             const amt = (document.getElementById('amt') as HTMLInputElement).value;
             const msg = (document.getElementById('msg') as HTMLInputElement).value;
-            
+
             if (validate(number, amt, msg)) {
               const editAmt = (document.getElementById('editAmt') as HTMLInputElement).checked;
               const editMsg = (document.getElementById('editMsg') as HTMLInputElement).checked;
